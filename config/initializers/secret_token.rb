c4184b892c71588d9586d68e108e19ccb484200e
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-02-18 16:58:07 +0100

    initial commit for the website management system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteManagement::Application.config.secret_token = 'fb12b4a73b2344b4cb41ad9441a3c10d0bfcf1d97528e999bb23d782e123f3d7cb72bf6baed9cff22757a582aa5f5bcc98767a16f88ad99bb848d01260f4dcf2'
