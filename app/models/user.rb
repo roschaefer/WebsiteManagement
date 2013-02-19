4a4081089bfcd90488df58f9e85c1a46e1756c03
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-02-19 12:51:01 +0100

    added name to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+
+  ###
+  attr_accessible :name
 end
