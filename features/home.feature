f1327c340dadfc24985ea45c41f377915de67f6e
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-02-22 16:14:20 +0100

    refactored cucumber static website feature changed file step definitions, so these files will be written

--- features/home.feature
+++ features/home.feature
@@ -1,7 +1,7 @@
 Feature: Show Home
     If I visit the home page I should see a greeting
+    This is just a hello world test for cucumber
 
-    # serves as a 'hello world' cucumber test
     Scenario: Navigate to Home
         Given I am on the home page
         Then I should see "Welcome!"
--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -1,9 +1,8 @@
 Given /^the following files are located in the websites folder?$/ do |table|
   table.hashes.each do |hash|
     pathname = Rails.root.join('app','views','websites', hash[:path])
-    File.exist?(pathname).should be_true
-    File.open(pathname, 'rb') do |f|
-      f.read.should be_eql(hash[:content])
-    end
+    # TODO: implement a careful file creation and clean up eventually
+    # File.exist?(pathname).should be_false
+    File.open(pathname, 'w') {|file| file.write(hash[:content])}
   end
 end
