d0375b9d9619ce801a64438c833299927d0434b4
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2013-04-01 02:58:07 +0200

    huge commit:     removed feature to signup as a client autonomously (devise     "registerable", "confirmable")     wrote test to ensure that an admin can create a user     adapted factories and migrations     created user mailer and let it send welcome messages with     credentials

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,8 +1,13 @@
 ActiveAdmin.register User do
+  random_password = (0...12).map{(65+rand(26)).chr}.join
   form do |f|
     f.inputs "Details" do
       f.input  :name
       f.input  :email
+      if f.object.new_record?
+        f.input  :password, :as => :hidden, :input_html => { :value => random_password}
+        f.input  :password_confirmation, :as => :hidden, :input_html => { :value => random_password}
+      end
       f.input :role, :as => :select, :collection => User::ROLES
     end
   f.actions
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :token_authenticatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :lockable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  after_create :send_credentials_email
 
   ROLES = %w[client admin]
 
@@ -19,5 +18,9 @@ class User < ActiveRecord::Base
   def role?(base_role)
     return false unless role # A user have a role attribute. If not set, the user does not have any roles.
     ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
+
+  def send_credentials_email
+    UserMailer.credentials_mail(self).deliver
   end
 end
